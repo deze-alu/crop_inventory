4ad8bf3440946068ff306472164503d54a051527
taiwodotunx1 <a.taiwo@alustudent.com>
2026-07-26 17:45:30 +0200

    Add add_crop function

--- features.py
+++ features.py
@@ -14,3 +14,14 @@ def view_inventory(inventory):
         crop = Crop.from_row(row)
         print(crop)
 
+
+def add_crop(inventory):
+    name = menu.get_text("Crop name: ")
+    planting_date = menu.get_text("Planting date (YYYY-MM-DD): ")
+    harvest_date = menu.get_text("Expected harvest date (YYYY-MM-DD): ")
+    status = menu.get_text("Status (e.g. Planted): ")
+    quantity = menu.get_number("Quantity planted: ")
+
+    new_id = inventory.add(name, planting_date, harvest_date, status, quantity)
+    print(f"Crop added successfully. Assigned ID: {new_id}")
+
